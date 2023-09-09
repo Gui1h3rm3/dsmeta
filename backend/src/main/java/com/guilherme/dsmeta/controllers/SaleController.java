3b337a96213942491d2328f238d12a9f298b1f5c
guilherme <guilhermebha1989@gmail.com>
2023-09-09 13:46:04 -0300

    Date select

--- backend/src/main/java/com/guilherme/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/guilherme/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.guilherme.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.dsmeta.entities.Sale;
@@ -17,9 +18,11 @@ public class SaleController {
 	@Autowired
 	private SaleService saleService;
 	
-	@GetMapping(value = "/findAll")
-	public List<Sale> findSales() {
-		return this.saleService.findSales();
+	@GetMapping(value = "/findSales")
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return this.saleService.findSales(minDate, maxDate, pageable);
 	}
-		
 }
